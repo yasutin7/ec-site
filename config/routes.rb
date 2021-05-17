f6813f88aac268c82aea1ea0656626c99d24a93d
yasunari <heaty2525@yahoo.co.jp>
2021-05-17 15:11:25 +0900

    練習

--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,6 @@ Rails.application.routes.draw do
     patch '/orders/:id/item_status' => 'orders#item_status_update', as: "item_status" # 製作ステータスupdate
   end
   #customer
-  root 'homes#top'
-  get '/thanks' => 'homes#thanks' #サンクスページ
-  get '/orders/confirm' => 'orders#confirm', as: 'orders_confirm' #購入確認画面への遷移
-  get '/orders/create_order' => 'orders#create_order' #購入確定のアクション
-  post '/orders/create_ship_address' => 'orders#create_ship_address' #情報入力画面での配送先登録用のアクション
-  delete '/cart_items' => 'cart_items#destroy_all' #カートアイテムを全て削除
-  resources :customers, only: [:show, :edit, :update]
   get '/customers/:id/withdrow' => 'customers#withdrow', as: 'withdrow_customer' #退会画面への遷移
   patch '/customers/:id/withdrow' => 'customers#switch', as: 'withdrow_switch_customer' #会員ステータスの切替
   resources :ship_addresses, except: [:new, :show]
